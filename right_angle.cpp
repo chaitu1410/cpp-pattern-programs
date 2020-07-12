89784d153cf6cc91884eca2c76286bd3812a26b4
Shinde Chaitanya Adinath <46563731+chaitu1410@users.noreply.github.com>
2020-07-12 02:36:23 -0700

    Update right_angle.cpp

--- right_angle.cpp
+++ right_angle.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(){
     //ios_base::fmtflags flags = cout.flags();
-    cout.setf(ios::right);
+    cout.setf(ios::right);              /////// Justify output text to right side.(By default output text is left justified.)
     int counter = 1;
     for(int i = 0; i <= 3; i++){
         for(int j = 3; j >= 0; j--){
-            if(j<=i)cout<<setw(3)<<counter++;else cout<<setw(3)<<" ";
+            if(j<=i)cout<<setw(3)<<counter++;else cout<<setw(3)<<" ";  /////// Set width of each letter 3 for better arrangement.
         }
         cout<<endl;
     }
