37c46300da0a4e31e818f7d5426012a030e6c262
chaitu1410 <chaitushinde1410@gmail.com>
2020-07-12 14:53:33 -0700

    First two programs of reght angled triangle and star step pattern.

---
+++ right_angle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+/*
+int main(){
+    //ios_base::fmtflags flags = cout.flags();
+    cout.setf(ios::right);
+    int counter = 1;
+    for(int i = 0; i <= 3; i++){
+        for(int j = 3; j >= 0; j--){
+            if(j<=i)cout<<setw(3)<<counter++;else cout<<setw(3)<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+*/
---
+++ step_pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+/*
+int main(){
+    int lb=3,ub=4,c=1;
+    for(int i = 0; i <= 7; i++){
+        for(int j = 0; j <= 7; j++){
+            if(j >= ub-c && j <= lb+c){
+                cout<<" * ";
+            }
+            else{
+                cout<<"   ";
+            }
+        }
+        if(i%2 == 1){
+            c++;
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
+*/
